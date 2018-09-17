6c0b8c66e633f879a4997009813ffe735f63d656
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-17 22:56:57 +0000

    first commit

---
+++ lib/all/black/roster.rb
@@ -0,0 +1,9 @@
+require "all/black/roster/version"
+
+module All
+  module Black
+    module Roster
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/all/black/roster/version.rb
@@ -0,0 +1,7 @@
+module All
+  module Black
+    module Roster
+      VERSION = "0.1.0"
+    end
+  end
+end
